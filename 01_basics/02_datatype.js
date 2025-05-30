ba3ed3407066553510e55c594def44bcf50bc6bc
Satyam <satyamsri24@gmail.com>
2025-05-30 19:23:51 +0000

    data type

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,14 @@
+
+
+console.log(33+4);
+
+// Number => 1234
+// Boolean => true/false
+// String => name '' ""
+// null => standalong value
+// undefined => undefined
+
+console.log(typeof 'satyam');
+
+// type of null is Object
+// type of undefined is undefined
