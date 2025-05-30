8e06185beb94f6e5c0607e53a1dc08e1c49b896a
Satyam <satyamsri24@gmail.com>
2025-05-30 18:14:25 +0000

    const and variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId='satyam';
+let password=12345;
+var email='satyam@google.com';
+keyword=98765;
+let state;
+
+// we will not use Var keyword
+
+// accountId='shubham'; not executable 
+
+
+password=9898989;
+email='shubham@google.com';
+console.log(accountId);
+console.table([accountId,password,email,keyword,state])
