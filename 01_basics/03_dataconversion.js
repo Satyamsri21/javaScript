83948e4a39e50021e3746e3673df8da8613a931b
Satyam <satyamsri24@gmail.com>
2025-05-31 06:14:07 +0000

    dataconversion

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,27 @@
+let score=false;
+
+//console.log(typeof '34abc');
+
+//console.log(typeof('34abc'));
+
+let valueInNumber=Number(score);
+
+//console.log(valueInNumber);
+//console.log(typeof(valueInNumber));
+
+// '33' => number
+// '33abcd' => NaN
+// true=> 1 false => 0
+
+let booleanIn='';
+let booleanLoggedIn = Boolean(booleanIn);
+//console.log(booleanLoggedIn);
+
+// 1=> true 0 => false
+// 'satyam' => true
+// '' => false
+
+let someNumber=33;
+let someStringIn=String(someNumber);
+console.log(someStringIn);
+console.log(typeof (someStringIn));
