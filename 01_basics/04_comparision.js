df7f28cc47363d16d5bbc7095fc6a78944075abc
Satyam <satyamsri24@gmail.com>
2025-06-01 19:02:18 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(4>=2);
+// console.log(4<7);
+// console.log(5!=3);
+// console.log(3==3);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// comparision operator will convert null into 0 
+// <><=>=
+// null => 0
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+
+// ===
+
+console.log("2"==2);
+console.log("2"===2);
