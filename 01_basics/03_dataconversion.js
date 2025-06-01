19267aad3b16fd10faedc242ee2871a63213ecd7
Satyam <satyamsri24@gmail.com>
2025-06-01 19:02:08 +0000

    data conversion

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -23,5 +23,55 @@ let booleanLoggedIn = Boolean(booleanIn);
 
 let someNumber=33;
 let someStringIn=String(someNumber);
-console.log(someStringIn);
-console.log(typeof (someStringIn));
+// console.log(someStringIn);
+// console.log(typeof (someStringIn));
+
+
+// =============================Operations===============================
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+4);
+// console.log(4-2);
+// console.log(2*3);
+// console.log(2**3)
+// // 2*2*2
+// console.log(2/3);
+// console.log(2%3);
+
+let val1="Hello";
+let val2=" satyam"
+let val3=val1+val2;
+// console.log(val3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((2+4)-5*2/3);
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+// let val1,val2,val3;
+// val1=val2=val3=2+2;
+
+let gameOperation=100;
+++gameOperation;
+// console.log(gameOperation);
+
+// let x=3;
+// const y=x++;
+// console.log(x);
+// console.log(y);
+
+let x=4;
+const y =++x;
+console.log(x);
+console.log(y);
+
+// y = 1+4
