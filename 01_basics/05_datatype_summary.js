60a8dc9131789b16940f1b193b0fe49a4f1e8b43
Satyam <satyamsri24@gmail.com>
2025-06-07 07:03:40 +0000

    stack and Heap memory

--- 01_basics/05_datatype_summary.js
+++ 01_basics/05_datatype_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,32 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive) and heap(non- primitive)
+
+let youtubeName="codewise101";
+
+let anotherName=youtubeName;
+anotherName="codewise101satyam";
+console.log(youtubeName);
+console.log(anotherName);
+
+let userOne={
+    userName:"satyam",
+    userId:12345,
+}
+
+let userTwo=userOne;
+
+userTwo.userName="satyamSri";
+
+console.log(userOne.userName);
+console.log(userTwo.userName);
